81207fe138a9a511bfc2ab56ce5e43952a472568
Till Wegmüller <toasterson@gmail.com>
2016-04-22 22:30:03 +0200

    Removed: known catalog as 90% of data in there seems to be grabage that needs to be cleared. Dont import reload from server. If that helps Added: pkg5 catalog import

--- libpkg/Catalog.cpp
+++ libpkg/Catalog.cpp
@@ -7,11 +7,11 @@
 #include "Catalog.h"
 #define BOOST_SYSTEM_NO_DEPRECATED
 #include <boost/filesystem.hpp>
-#include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/info_parser.hpp>
 
 namespace fs = boost::filesystem;
+
 
 void pkg::Catalog::upgrade_format(const std::string &newRoot) {
 
@@ -32,25 +32,68 @@ pkg::Catalog::Catalog(const std::string &root, const std::string &catalogtype, c
 }
 
 void pkg::Catalog::importpkg5() {
-    boost::property_tree::ptree catalog_attrs, catalog_base, catalog_dependency, catalog_summary;
-    boost::property_tree::read_json(root_dir+"/catalog.attrs", catalog_attrs);
-    boost::property_tree::read_json(root_dir+"/catalog.base.C", catalog_base);
-    boost::property_tree::read_json(root_dir+"/catalog.dependency.C", catalog_dependency);
-    boost::property_tree::read_json(root_dir+"/catalog.summary.C", catalog_summary);
+    using namespace boost::property_tree;
+
+    ptree catalog_attrs, catalog_base, catalog_dependency, catalog_summary;
+    read_json(root_dir+"/catalog.attrs", catalog_attrs);
+    read_json(root_dir+"/catalog.base.C", catalog_base);
+    read_json(root_dir+"/catalog.dependency.C", catalog_dependency);
+    read_json(root_dir+"/catalog.summary.C", catalog_summary);
 
     data.put("package-count", catalog_attrs.get<std::string>("package-count"));
     data.put("version", 2);
-    boost::property_tree::ptree::iterator end = catalog_base.end();
-    for (boost::property_tree::ptree::iterator publisher = catalog_base.begin(); publisher != end; ++publisher){
-        std::string publisher_name = publisher->first;
-        boost::property_tree::ptree::iterator pub_end = publisher->second.end();
-        for(boost::property_tree::ptree::iterator package = publisher->second.begin(); package != pub_end; ++package){
-            std::string package_name = package->first;
-            boost::property_tree::ptree::iterator package_end = package->second.end();
-            for(boost::property_tree::ptree::iterator package_version = package->second.begin(); package_version != package_end; ++package_version){
+
+    for (auto publisher : catalog_base){
+        std::string publisher_name = publisher.first;
+
+        for(auto package : publisher.second){
+            std::string package_name = package.first;
+
+            for(auto package_version : package.second){
                 std::string base_path = publisher_name+"\\"+package_name;
-                std::string data_path = base_path+"\\"+package_version->second.get<std::string>("version");
-                data.put(boost::property_tree::ptree::path_type(data_path+"\\signature-sha-1", '\\'), package_version->second.get<std::string>("signature-sha-1"));
+                std::string data_path = base_path+"\\"+package_version.second.get<std::string>("version");
+                data.put(ptree::path_type(data_path+"\\signature-sha-1", '\\'), package_version.second.get<std::string>("signature-sha-1"));
+
+                ptree state_tree = ptree();
+                for (auto package_state : package_version.second.get_child("metadata.states")){
+                    state_tree.put(package_state.second.get<std::string>(""), true);
+                }
+                data.put_child(ptree::path_type(data_path+"\\states", '\\'), state_tree);
+
+                //TODO maybe i want the dependencies listed differently in my catalog version need to decide
+
+
+                boost::optional<ptree&> check_package_dependency = catalog_dependency.get_child_optional(ptree::path_type(base_path, '\\'));
+                if(check_package_dependency) {
+                    ptree package_dependency = catalog_dependency.get_child(ptree::path_type(base_path, '\\'));
+                    for (auto package_dependency_version: package_dependency) {
+                        int dependency_count = 1;
+                        if (package_version.second.get<std::string>("version") ==
+                            package_dependency_version.second.get<std::string>("version")) {
+                            for (auto package_dependency_action: package_dependency_version.second.get_child("actions")) {
+                                data.put(ptree::path_type(data_path+"\\dependencies\\"+std::to_string(dependency_count), '\\'), package_dependency_action.second.get<std::string>(""));
+                                dependency_count++;
+                            }
+                            dependency_count = 1;
+                        }
+                    }
+                }
+
+                boost::optional<ptree&> check_package_summary = catalog_summary.get_child_optional(ptree::path_type(base_path, '\\'));
+                if(check_package_summary) {
+                    ptree package_summary = catalog_summary.get_child(ptree::path_type(base_path, '\\'));
+                    for (auto package_summary_version: package_summary) {
+                        int action_count = 1;
+                        if (package_version.second.get<std::string>("version") ==
+                                package_summary_version.second.get<std::string>("version")) {
+                            for (auto package_summary_action: package_summary_version.second.get_child("actions")) {
+                                data.put(ptree::path_type(data_path+"\\actions\\"+std::to_string(action_count), '\\'), package_summary_action.second.get<std::string>(""));
+                                action_count++;
+                            }
+                            action_count = 1;
+                        }
+                    }
+                }
             }
         }
     }
