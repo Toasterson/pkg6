4a5b79deaff0cb863d5cb6efc7adef4ef28e4449
Till Wegmüller <toasterson@gmail.com>
2016-04-23 11:35:13 +0200

    Added: Progress Output class

---
+++ libpkg/Progress.h
@@ -0,0 +1,50 @@
+//
+// Copyright (c) Till Wegmueller 2016 under CDDL
+// for License see LICENSE file in root of repository
+//
+
+#ifndef PKG6_PROGRESS_H
+#define PKG6_PROGRESS_H
+
+#include <iostream>
+
+class Progress {
+
+private:
+    std::ostream& stream;
+    int max;
+    int current;
+    std::string pre_text;
+    std::string post_text;
+    void writeStatus(){
+        stream << '\r' << pre_text << '\t' << current << '/' << max << '\t' << post_text << std::flush;
+    }
+public:
+    Progress(const std::string& pre_text = "", const std::string& post_text = "", const int& maximum = 100):
+            stream(std::cout),
+            pre_text(pre_text),
+            post_text(post_text),
+            max(maximum),
+            current(0)
+    { }
+    Progress(std::ostream& outstream, const std::string& pre_text = "", const std::string& post_text = "", const int& maximum = 100):
+            stream(outstream),
+            pre_text(pre_text),
+            post_text(post_text),
+            max(maximum),
+            current(0)
+    { }
+    void setMax(const int& max){this->max=max;}
+    int operator+=(const int& count){
+        current += count;
+        writeStatus();
+        return current;
+    }
+
+    int operator++(int){ return operator+=(1);}
+
+
+};
+
+
+#endif //PKG6_PROGRESS_H
